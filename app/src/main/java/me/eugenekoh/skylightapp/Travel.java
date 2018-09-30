df864615b1551f53d48eedecc7cbed38059edc1c
Lim Xiao Qi <xqlim1996@gmail.com>
2018-09-30 12:12:51 +0800

    integrated v1

---
+++ app/src/main/java/me/eugenekoh/skylightapp/Travel.java
@@ -0,0 +1,13 @@
+package me.eugenekoh.skylightapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Travel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel);
+    }
+}
